be234a66e0faf47c41ae665f8326855bc04f0915
Cate <ceh2181@columbia.edu>
2020-11-19 08:54:20 -0500

    Successfully fetch terms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
+/**
+ * Homepage is where program data is fetched. User can select a program
+ * here and opt to view payment terms.
+ */
 const Home = () => {
-  return <div>Home page</div>;
+  const [terms, setTerms] = useState({});
+
+  useEffect(() => {
+    const fetchTerms = async () => {
+      const result = await axios('https://api.wetradeup.com/terms');
+
+      setTerms(result.data);
+    };
+
+    fetchTerms();
+  }, []);
+
+  console.log(terms);
+
+  const programs = Object.keys(terms);
+
+  console.log(programs);
+
+  //e.g. if data has not yet been fetched
+  if (terms === null) {
+    return <div />;
+  }
+
+  return (
+    <React.Fragment>
+      <div>Home page</div>
+      <Link to="terms">See my terms</Link>
+    </React.Fragment>
+  );
 };
 
 export default Home;
